7e02ff1da6237585b9578cc405f6e063add5ecf8
Bo Brockmann <bbrockmann1@gmail.com>
2022-09-22 14:52:55 -0600

    commit tree b699dbc3d22af52d27c26cc6b77484c445090f44

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(name, email){
+    const formData = {
+    name: "Steve",
+    email: "steve@steve.com"
+};
+    
+    const configObj = {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(formData)
+}
+    return fetch('http://localhost:3000/users', configObj)
+    .then(resp => {return resp.json()})
+    .then((obj) => {
+        document.body.textContent = obj["id"]
+    })
+    .catch(error => {
+        document.body.textContent = error.message;
+    })
+};
